0217692e274db1168017da9248a8a598b914f89e
Denis Senchishen <dsenchishen@icloud.com>
2023-07-01 15:13:28 +0300

    feat: add persisting data to pickle, significantly increase performance

--- data/data.py
+++ data/data.py
@@ -1,6 +1,7 @@
+import pandas as pd
 import os
 
-import pandas as pd
+DATA_FILE_PATH = "kodifikator_data.pkl"
 
 
 def load_xlsx() -> pd.DataFrame:
@@ -11,27 +12,32 @@ def load_xlsx() -> pd.DataFrame:
 
 
 def load_data() -> pd.DataFrame:
-    df = load_xlsx()
-    df["id"] = df["Додатковий рівень"].fillna(
-        df["Четвертий рівень"].fillna(
-            df["Третій рівень"].fillna(
-                df["Другий рівень"].fillna(df["Перший рівень"])
+    if os.path.exists(DATA_FILE_PATH):
+        df = pd.read_pickle(DATA_FILE_PATH)
+    else:
+        df = load_xlsx()
+        df["id"] = df["Додатковий рівень"].fillna(
+            df["Четвертий рівень"].fillna(
+                df["Третій рівень"].fillna(
+                    df["Другий рівень"].fillna(df["Перший рівень"])
+                )
             )
         )
-    )
-    category_mapping = {
-        "O": "Області та АРК",
-        "K": "Міста зі спеціальним статусом (Київ та Севастополь)",
-        "P": "Райони в областях та в АРК",
-        "H": "Територіальні громади",
-        "M": "Міста",
-        "T": "Селища міського типу",
-        "C": "Села",
-        "X": "Селища",
-        "B": "Райони в містах",
-    }
+        category_mapping = {
+            "O": "Області та АРК",
+            "K": "Міста зі спеціальним статусом (Київ та Севастополь)",
+            "P": "Райони в областях та в АРК",
+            "H": "Територіальні громади",
+            "M": "Міста",
+            "T": "Селища міського типу",
+            "C": "Села",
+            "X": "Селища",
+            "B": "Райони в містах",
+        }
 
-    df["Назва категорії об’єкта"] = df["Категорія об’єкта"].map(
-        category_mapping
-    )
+        df["Назва категорії об’єкта"] = df["Категорія об’єкта"].map(
+            category_mapping
+        )
+        df.to_pickle(DATA_FILE_PATH)  # Persist the DataFrame to disk
+
     return df
