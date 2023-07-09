c3c669e98f8763d10c75c694a5aca98089eaad60
Denis Senchishen <dsenchishen@icloud.com>
2023-07-10 00:56:08 +0300

    fix: update setup.py config

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup
 
-setup()
+setup(
+    name='katottglib',
+    version='0.2.1',
+    packages=['katottglib'],
+    install_requires=[
+    ],
+    author='Denis Senchishen',
+    author_email='dsenchishen@icloud.com',
+    description='A Python lib to operate KATOTTG addresses (classified administrative division of Ukraine).',
+    url='https://github.com/sen-den/katottglib',
+)
